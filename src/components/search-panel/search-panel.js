bd9dc07f5b3abccecdbe005dab687a5232004ff8
Yevhen Moiseienko <mojseenko2011@gmail.com>
2022-04-09 17:50:45 +0300

    Greate visual interface without logic

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,12 @@
+import './search-panel.css'
+
+const SearchPanel = () => {
+    return (
+        <input
+            type="text"
+            className="form-control search-input"
+            placeholder="Введите имя сотрудника"/>
+    )
+}
+
+export default SearchPanel;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import {createRoot} from "react-dom/client";
 import './index.css';
 import App from "./components/app/app";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
 
