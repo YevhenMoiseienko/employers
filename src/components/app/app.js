52499b857f741404f4371cf94d98a81056096541
Yevhen Moiseienko <mojseenko2011@gmail.com>
2022-04-12 08:52:58 +0300

    Include Add and Delete employers logick

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
+import {Component} from "react";
 
 import './app.css'
 import AppInfo from "../app-info/app-info";
@@ -7,25 +8,53 @@ import AppFilter from "../app-filter/app-filter";
 import EmployersList from "../employers-list/employers-list";
 import EmployersAddForm from "../employers-add-form/employers-add-form";
 
-const App = () => {
+class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {id: uuidv4(), name: "John Smith", salary: 1000, increase: false},
+                {id: uuidv4(), name: "Alex Mur", salary: 3000, increase: false},
+                {id: uuidv4(), name: "Tony Montana", salary: 800, increase: true}
+            ]
+        }
+    }
 
-    const data = [
-        {id: uuidv4(),name: "John Smith", salary: 1000, increase: false},
-        {id: uuidv4(),name: "Alex Mur", salary: 3000, increase: false},
-        {id: uuidv4(),name: "Tony Montana", salary: 800, increase: true}
-    ]
+    onDelete = (id) => {
+        this.setState(({data}) => ({
+            data: data.filter(item => item.id !== id)
+        }))
+    }
 
-    return (
-        <div className="app">
-            <AppInfo/>
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+    addItem = (name, salary) => {
+
+        const newItem = {
+            name,
+            salary,
+            id: uuidv4(),
+            increase: false
+        }
+
+        this.setState(({data}) => ({
+            data: [...data, newItem]
+        }))
+    }
+
+
+    render() {
+        const {data} = this.state
+        return (
+            <div className="app">
+                <AppInfo/>
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+                <EmployersList data={data} onDelete={this.onDelete}/>
+                <EmployersAddForm onAdd={this.addItem}/>
             </div>
-            <EmployersList data={data}/>
-            <EmployersAddForm/>
-        </div>
-    )
+        )
+    }
 }
 
 export default App;
--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -3,11 +3,11 @@ import './employers-list.css'
 
 import EmployersListItem from "../employers-list-item/employers-list-item";
 
-const EmployersList = ({data}) => {
+const EmployersList = ({data, onDelete}) => {
 
     const elements = data.map(item => {
         const {id, ...itemProps} = item
-        return <EmployersListItem key={id} {...itemProps}/>
+        return <EmployersListItem onDelete={() => onDelete(id)} key={id} {...itemProps}/>
     })
 
     return (
